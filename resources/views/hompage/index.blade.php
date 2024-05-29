75598ae68c7650415eb608542a1714c390f7a7a3
SASwagoto <saswagoto@gmail.com>
2024-05-29 14:17:33 +0600

    first commit

--- resources/views/hompage/index.blade.php
+++ resources/views/hompage/index.blade.php
@@ -10,7 +10,7 @@
 <div class="flex justify-center items-center mt-5">
     <form action="{{route('search')}}" class="w-full flex justify-center items-center" method="GET">
         <div class="w-1/3 flex relative">
-            <input type="text" class="w-full py-4 px-5 rounded-full border border-gray hover:drop-shadow-md focus:drop-shadow-md" placeholder="Search" name="keyword">
+            <input type="text" class="w-full py-4 px-5 rounded-full border border-gray hover:drop-shadow-md focus:outline-none focus:drop-shadow-md" placeholder="Search" name="keyword">
             <button type="submit" class="absolute top-1/2 right-2 -translate-y-1/2 p-2">
                 <i class="fa-solid fa-magnifying-glass fa-xl"></i>
             </button>
